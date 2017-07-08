f1372e470ce2808f2f75745aaf679b310920de9e
Nate Miller <nathanmiller9@yahoo.com>
2017-07-07 18:52:36 -0700

    udpated basic landing page template to include labels

--- src/components/main/Advanced.js
+++ src/components/main/Advanced.js
@@ -7,7 +7,7 @@ class Advanced extends Component {
     return (
       <div className="advanced">
          <Col xs={12} sm={3} md={3} lg={3} xl={3}>
-				<p>Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced 
+				<p>Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced Advanced
 				</p>
 		</Col>
       </div>
