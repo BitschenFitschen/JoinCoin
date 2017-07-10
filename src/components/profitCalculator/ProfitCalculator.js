5049a4434b3407c6eeb4aac6f8b55e463b323a06
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-07-10 15:09:31 -0700

    add starter files for profit calculator

---
+++ src/components/profitCalculator/ProfitCalculator.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {Col, Accordion, Panel} from 'react-bootstrap';
+import './profit.css';
+
+class ProfitCalculator extends Component {
+  render () {
+
+    return (
+     
+    );
+  }
+}
+
+export default Education;
