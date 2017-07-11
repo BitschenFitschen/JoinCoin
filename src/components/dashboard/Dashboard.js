f5d13a5bf121e715ff19dc597cc277e91a58ef6f
karunashi <karunashi@gmail.com>
2017-07-11 16:00:22 -0700

    Added NPM axios, and preliminary test with GET. Data pulled for BTC-USD conversion on a full ticker on cryptonator, and verified on the console.

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 import {Col} from 'react-bootstrap';
+import axios from 'axios';
 
 class Dashboard extends Component {
+  componentWillMount() {
+    axios.get('https://api.cryptonator.com/api/full/btc-usd')
+      .then(function (response) {
+    	console.log(response.data.ticker.base)
+      })
+      .catch(function (errorMsg) {
+        console.log(errorMsg);
+      });
+  }
+
+
   render() {
     return (
       <div className="dashboard">
