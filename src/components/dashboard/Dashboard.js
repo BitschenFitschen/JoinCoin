28065a4f4fb27be7887a8815d850ed3b5d62c69c
karunashi <karunashi@gmail.com>
2017-07-11 16:11:26 -0700

    Added constructor and set up the props/state to ensure the data would display shortly in render. Working on rendering now.

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,14 +3,26 @@ import {Col} from 'react-bootstrap';
 import axios from 'axios';
 
 class Dashboard extends Component {
-  componentWillMount() {
+   constructor(props) {
+   	super(props)
+   	this.state = {
+   		dashboardData: 9001
+   	}
+   }
+
+   componentWillMount() {
+    var _this = this
     axios.get('https://api.cryptonator.com/api/full/btc-usd')
       .then(function (response) {
-
-     	for (var i = 0; i < 10; i++) {
+        _this.setState({
+    		dashboardData: response.data.ticker.base
+    	})
+      	for (var i = 0; i < 10; i++) {
         console.log(response.data.ticker.markets[i]);
     }
     	console.log(response.data.ticker.base)
+    	return response.data
+
       })
       .catch(function (errorMsg) {
         console.log(errorMsg);
