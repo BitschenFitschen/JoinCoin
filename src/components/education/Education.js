a4f6b7083a3457c35cff5bae198180f83d31e622
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-07-11 12:35:22 -0700

    preload slide info from external json for easy data editing

--- src/components/education/Education.js
+++ src/components/education/Education.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Col, Accordion, Panel} from 'react-bootstrap';
 import Slider from 'react-slick';
 import './education.css';
+import preload from './educationdata.json';
 
 class Education extends Component {
   render () {
@@ -14,54 +15,26 @@ class Education extends Component {
         <Col xs={12} sm={12} md={12} lg={12} xl={12}>
           <h1>Education</h1>
           <Slider {...settings}>
-            <div className='education-panel'>
-              <Col md={3} />
-              <Col md={6}>
-                <h3>Would you like to learn about cryptocurrencies?</h3>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-              </Col>
-              <Col md={3} />
-            </div>
-            <div className='education-panel'>
-              <Col md={3} />
-              <Col md={6}>
-                <h3>Blockchain Tech</h3>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-              </Col>
-              <Col md={3} />
-            </div>
-            <div className='education-panel'>
-              <Col md={3} />
-              <Col md={6}>
-                <h3>Trust and Third Parties</h3>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-              </Col>
-              <Col md={3} />
-            </div>
-            <div className='education-panel'>
-              <Col md={3} />
-              <Col md={6}>
-                <h3>Value in Decentralization</h3>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-              </Col>
-              <Col md={3} />
-            </div>
-            <div className='education-panel'>
-              <Col md={3} />
-              <Col md={6}>
-                <h3>Future of Money</h3>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-              </Col>
-              <Col md={3} />
-            </div>
-            <div className='education-panel'>
-              <Col md={3} />
-              <Col md={6}>
-                <h3>Gold and Money Comparisons</h3>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-              </Col>
-              <Col md={3} />
-            </div>
+            {
+              preload.slides.map(
+                (slide) =>
+                  <div className='education-panel'>
+                    <Col md={3}>
+                      <h4>{slide.leftSidePanelTitle}</h4>
+                      <small>{slide.leftSidePanelDescription}</small>
+                    </Col>
+                    <Col md={6}>
+                      <h3>{slide.title}</h3>
+                      <p>{slide.description}</p>
+                    </Col>
+                    <Col md={3}>
+                      <h4>{slide.rightSidePanelTitle}</h4>
+                      <small>{slide.rightSidePanelDescription}</small>
+                    </Col>
+                  </div>
+              )
+            }
+
             <div className='education-panel'>
               <Col md={3} />
               <Col md={6}>
