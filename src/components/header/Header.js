1cef01783722dbb28b26e69dc98204edadf31124
Skyler La <yenla@Skylers-MacBook-Pro.local>
2017-07-11 19:32:02 -0700

    minor change to correct error on console

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,11 +10,6 @@ class Header extends Component {
 				  <h1>JoinCoin</h1>
 				  <h3>..the junction for all cryptocurrency</h3>
 		    </Col>
-      <div className='header'>
-        <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-          <h1>JoinCoin</h1>
-          <h3>..the junction for all cryptocurrency</h3>
-        </Col>
       </div>
     );
   }
