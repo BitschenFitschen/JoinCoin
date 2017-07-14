5ebbfe0494f0aefdcdb6b861b2ed81a1593123ac
karunashi <karunashi@gmail.com>
2017-07-13 17:29:05 -0700

    Added a few comments, also notated and moved around some experimental works I did with JSX/React syntax to fiddle with the data (which should be notated out but available for my own reminder later).

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,8 +3,8 @@ import {Col} from 'react-bootstrap';
 import axios from 'axios';
 
 class Dashboard extends Component {
-  
-  ////// Debug Code here
+
+
   // getInitialState(){
   //   return {
   //     render1: {
@@ -18,44 +18,47 @@ class Dashboard extends Component {
   //   }
   // }
 
-
    constructor(props) {
-   	super(props)
-   	this.state = {
-   		render1: 9001
-   	}
+    super(props)
+    this.state = {
+      render1: 9001
+    }
    }
 
    componentWillMount() {
-   	let renderArr1 = [];
+    let renderArr1 = [];
     let renderArr2 = [];
     let _this = this
     axios.get('https://api.coinmarketcap.com/v1/ticker/')
       .then(function (response) {
-      	for (var i = 0; i < 10; i++) {
-        renderArr1.push(<p key={i}>{response.data[i].name}</p>);
-        renderArr2.push(<p key={i}>{response.data[i].price_usd}</p>);
-    }
+        for (var i = 0; i < response.data.length; i++){
+          // console.log(response.data[i].id);
+          renderArr1.push(<p key={i}>{response.data[i].name}</p>);
+          renderArr2.push(<p key={i}>{response.data[i].price_usd}</p>);
+        }
+        console.log(response.data[0])
+        _this.setState({
+          fillerData1: renderArr1,
+          fillerData2: renderArr2,
+          id: response.data[0].id,
+          name: response.data[0].name,
+          symbol: response.data[0].symbol,
+          priceUS: response.data[0].price_usd,
+          perChange1h: response.data[0].percent_change_1h,
+          perChange24h: response.data[0].percent_change_24h,
+          perChange7d: response.data[0].percent_change_7d
 
-    _this.setState({
-      fillerData1: renderArr1,
-      fillerData2: renderArr2,
-      id: response.data[0].id,
-      name: response.data[0].name,
-      symbol: response.data[0].symbol,
-      priceUS: response.data[0].price_usd,
-      perChange1h: response.data[0].percent_change_1h,
-      perChange24h: response.data[0].percent_change_24h,
-      perChange7d: response.data[0].percent_change_7d
-    })
+        })
+
       })
       .catch(function (errorMsg) {
         console.log(errorMsg);
       });
+
+
   }
 
   render() {
-// Place for setStates later 
     let r1 = this.state.id
     let r2 = this.state.name
     let r3 = this.state.symbol
@@ -65,9 +68,10 @@ class Dashboard extends Component {
     let r7 = this.state.perChange7d
     let fillerData1 = this.state.fillerData1
     let fillerData2 = this.state.fillerData2
+    // let render2 = this.state.price
     return (
       <div className="dashboard">
-<Col xs={12} sm={12} md={6} lg={6} xl={6}>
+        <Col xs={12} sm={12} md={6} lg={6} xl={6}>
         {
           <div>
           <h1>Search Query Results [BTC as Example]</h1>
@@ -102,13 +106,21 @@ class Dashboard extends Component {
         }
         </Col>
       </div>
+         // Testing Mapping to pull data from array of objects
+         // render1.map((objMap, index) => {
+         //  return <p key={index}>{objMap}</p>
+         // })
+
     );
   }
 }
-  // Testing Mapping to pull data from array of objects
-  // render1.map((objMap, index) => {
-  //  return <p key={index}>{objMap}</p>
-  // })
 
+  // componentDidMount () {
+  //  fetch('https://api.cryptonator.com/api/full/btc-usd')
+ //     .then(function(response) { return response.json(); })
+ //     .then(function(json) {
+ //     console.log(json);
+  //  });
+  // }
 
 export default Dashboard;
