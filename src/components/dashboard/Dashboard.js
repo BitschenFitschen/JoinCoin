4bc64d1fd16a4a0b2407ae5783f5a5ff1c3a59a6
karunashi <karunashi@gmail.com>
2017-07-13 17:14:33 -0700

    Adding the first item from the for-loop that should have pulled the list up at that certain point (which should be less reliant on the API call limits once DB is set up to take API calls from Axios. Preliminary rendering.

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -33,9 +33,12 @@ class Dashboard extends Component {
     axios.get('https://api.coinmarketcap.com/v1/ticker/')
       .then(function (response) {
       	for (var i = 0; i < 10; i++) {
-
+        renderArr1.push(<p key={i}>{response.data[i].name}</p>);
     }
 
+    _this.setState({
+      fillerData1: renderArr1
+    })
       })
       .catch(function (errorMsg) {
         console.log(errorMsg);
@@ -44,12 +47,12 @@ class Dashboard extends Component {
 
   render() {
 // Place for setStates later 
-
+  let fillerData1 = this.state.fillerData1
     return (
       <div className="dashboard">
         <Col xs={12} sm={12} md={12} lg={12} xl={12}>
 				  {
-
+            {fillerData1}
           }
 		    </Col>
       </div>
