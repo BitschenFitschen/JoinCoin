a55a20b99cad82ffb974b2b11f0cce856fc38553
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-07-13 22:38:22 -0700

    display ticker symbol and premined info for each coin in multicoin pg, also begin adding onClick functionality to prep for modal

--- src/components/coins/Coin.js
+++ src/components/coins/Coin.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import {Col, Accordion, Panel} from 'react-bootstrap';
 import './coin.css';
 // import axios from 'axios';
 
-const Coin = (props) => (
-  <div className='coin-container'>
-    <h5>{props.coin.CoinName}</h5>
-  </div>
-);
+class Coin extends Component {
+  constructor (props) {
+    super(props);
+    this.handleCoinClick = this.handleCoinClick.bind(this);
+  }
+
+  handleCoinClick (event) {
+    console.log(this.props.coin.Name);
+  }
+
+  render () {
+    let premined = null;
+
+    if (this.props.coin.FullyPremined === '1') {
+      premined = <p>Premined</p>;
+    }
+
+    return (
+      <a href='#' onClick={(e) => this.handleCoinClick(e)}>
+        <div className='coin-container'>
+
+          <h5>{this.props.coin.CoinName}</h5>
+          <p>({this.props.coin.Name})</p>
+          {premined}
+        </div>
+      </a>
+    );
+  }
+}
 
 export default Coin;
--- src/components/coins/Coins.js
+++ src/components/coins/Coins.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 // import {Col, Accordion, Panel} from 'react-bootstrap';
+import './reset.css'
 import './coins.css';
 import Coin from './Coin';
 import axios from 'axios';
@@ -8,15 +9,42 @@ class Coins extends Component {
   constructor(props) {
     super(props);
     this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+    this.preminedOnly = this.preminedOnly.bind(this);
+    this.notPreminedOnly = this.notPreminedOnly.bind(this);
+    this.nullPremined = this.nullPremined.bind(this);
   }
 
   state = {
     coins: [],
-    searchTerm: ''
+    selectedCoin: '',
+    searchTerm: '',
+    filterPremined: null
   };
 
   handleSearchTermChange (event) {
     this.setState({ searchTerm: event.target.value });
+  }
+
+  preminedOnly (event) {
+    if(this.state.filterPremined === null) {
+      this.setState({ filterPremined: false });
+    } else if(this.state.filterPremined === true) {
+      this.setState({ filterPremined: false });
+    }
+  }
+
+  notPreminedOnly (event) {
+    if(this.state.filterPremined === null) {
+      this.setState({ filterPremined: true });
+    } else if(this.state.filterPremined === false) {
+      this.setState({ filterPremined: true });
+    }
+  }
+
+  nullPremined (event) {
+    if(this.state.filterPremined === true || this.state.filterPremined === false) {
+      this.setState({ filterPremined: null });
+    }
   }
 
   componentWillMount() {
@@ -24,7 +52,7 @@ class Coins extends Component {
     // this runs right before rendered
     axios.get('https://www.cryptocompare.com/api/data/coinlist/')
       .then(function (response) {
-        // console.log(response.data.Data);
+        console.log(response.data.Data);
         let arr = [];
 
         for (var key in response.data.Data) {
@@ -45,13 +73,16 @@ class Coins extends Component {
 
   render () {
     // console.log(Object.keys(this.state.coins));
-          
+
     return (
       <div className="container-fluid">
         <h1>Coins</h1>
         <input onChange={this.handleSearchTermChange} type="text" value={this.state.searchTerm} placeholder="Search for coin" />
+        <button onClick={(e) => this.preminedOnly(e)}>Premined Coins</button>
+        <button onClick={(e) => this.notPreminedOnly(e)}>Mineable Coins</button>
+        <button onClick={(e) => this.nullPremined(e)}>All Coins</button>
         <div className="coins-container">
-          {
+          { 
             this.state.coins
               .sort( (a, b) => {
                 var idA = a.SortOrder;
@@ -68,6 +99,11 @@ class Coins extends Component {
                 return 0;
               })
               .filter( coin => `${coin.FullName}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+              .filter( coin =>
+                ( ( coin.FullyPremined === "0" || coin.FullyPremined === "1" ) && this.state.filterPremined === null ) ||
+                ( coin.FullyPremined === "0"  && this.state.filterPremined === true ) ||
+                ( coin.FullyPremined === "1"  && this.state.filterPremined === false )
+              )
               .map( coin => (
               <Coin key={coin.Id} coin={coin} />
             ))
