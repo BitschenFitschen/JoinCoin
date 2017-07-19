322cde101c0501f0d7b6496e4da50c9348032001
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-07-19 00:42:29 -0700

    style multi-coins pg

--- src/components/coins/Coin.js
+++ src/components/coins/Coin.js
@@ -7,29 +7,58 @@ class Coin extends Component {
   constructor (props) {
     super(props);
     this.handleCoinClick = this.handleCoinClick.bind(this);
+    this.parseNum = this.parseNum.bind(this);
   }
 
   handleCoinClick (event) {
     console.log(this.props.coin.Name);
   }
 
+  parseNum (value) {
+    // Nine Zeroes for Billions
+    return Math.abs(Number(value)) >= 1.0e+9
+
+    ? Math.abs(Number(value)) / 1.0e+9 + "B"
+    // Six Zeroes for Millions 
+    : Math.abs(Number(value)) >= 1.0e+6
+
+    ? Math.abs(Number(value)) / 1.0e+6 + "M"
+    // Three Zeroes for Thousands
+    : Math.abs(Number(value)) >= 1.0e+3
+
+    ? Math.abs(Number(value)) / 1.0e+3 + "K"
+
+    : Math.abs(Number(value));
+  }
+
   render () {
     let premined = null;
 
     if (this.props.coin.FullyPremined === '1') {
-      premined = <span>Premined</span>;
+      premined = <div className='premined'>Premined</div>;
+    }
+
+    let coinSupply = null;
+
+    if (this.props.coin.TotalCoinSupply !== '0' && this.props.coin.TotalCoinSupply !== 'N/A') {
+      coinSupply = <div className='coin-supply'>{this.parseNum(this.props.coin.TotalCoinSupply)}</div>
+    }
+
+    let algo = null;
+
+    if (this.props.coin.Algorithm !== 'N/A') {
+      algo = <div className='algo'>{this.props.coin.Algorithm} </div>
     }
 
     return (
-      <a href='#' onClick={(e) => this.handleCoinClick(e)}>
+      <a href='#' onClick={(e) => this.handleCoinClick(e)} className="coin-link">
         <div className='coin-container'>
-
-          <span>{this.props.coin.CoinName} </span>
-          <span>({this.props.coin.Name}) </span>
-          <span>{"  |  Algorithm: "+ this.props.coin.Algorithm} </span>
-          <span>{"  |  Total Coin Supply: "+ this.props.coin.TotalCoinSupply} </span>
+          <div className='ranking text-center'>{`${this.props.coin.SortOrder}`}</div>
+          <div className='coin-name'>{this.props.coin.CoinName}</div>
+          <div className='coin-sym'>{this.props.coin.Name}</div>
+          {coinSupply}
           {premined}
-          <span>{ `| ${this.props.coin.SortOrder}`}</span>
+          {algo}
         </div>
       </a>
     );
