39e6d01189ee982012481ae862a8268eb41aba4e
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-07-26 12:03:37 -0700

    fix parsenum for profit calc

--- src/components/profitCalculator/ProfitCalculator.js
+++ src/components/profitCalculator/ProfitCalculator.js
@@ -132,21 +132,17 @@ class ProfitCalculator extends Component {
     this.setState({ payoff: parseInt(e.target.value, 10) });
   }
 
-  parseNum (value) {
-    // Nine Zeroes for Billions
-    return Math.abs(Number(value)) >= 1.0e+9
-
-    ? Math.abs(Number(value)) / 1.0e+9 + "B"
-    // Six Zeroes for Millions 
-    : Math.abs(Number(value)) >= 1.0e+6
-
-    ? Math.abs(Number(value)) / 1.0e+6 + "M"
-    // Three Zeroes for Thousands
-    : Math.abs(Number(value)) >= 1.0e+3
-
-    ? Math.abs(Number(value)) / 1.0e+3 + "K"
-
-    : Math.abs(Number(value));
+  parseNum (number) {
+    var SI_POSTFIXES = ["", "k", "M", "B", "T", "P", "E"];
+    var tier = Math.log10(Math.abs(number)) / 3 | 0;
+    if(tier === 0) return number;
+    var postfix = SI_POSTFIXES[tier];
+    var scale = Math.pow(10, tier * 3);
+    var scaled = number / scale;
+    var formatted = scaled.toFixed(1) + '';
+    if (/\.0$/.test(formatted))
+      formatted = formatted.substr(0, formatted.length - 2);
+    return formatted + postfix;
   }
 
   render () {
